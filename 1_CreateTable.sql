7300ff323e7b3b5cce5e0af5db994a96549c3ba7
Andrei Novikov <111170011+FoxnArt@users.noreply.github.com>
2023-09-11 21:37:55 +0300

    Add files via upload

---
+++ 1_CreateTable.sql
@@ -0,0 +1,11 @@
+create schema netolody;
+
+create table PERSONS
+(
+    name           varchar(50) not null,
+    surname        varchar(50) not null,
+    age            int not null,
+    phone_number   varchar(20) not null,
+    city_of_living varchar(60) not null default 'Moscow',
+    constraint PK_Person primary key (name, surname, age)
+);
